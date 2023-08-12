b68297562f74491590e5c47cfc141e5589e29443
Marcin <lmartino@wp.pl>
2023-08-12 08:53:07 +0200

    Initial commit

---
+++ src/main/java/pl/wszib/oemdatabase_test3/OemDataBaseTest3Application.java
@@ -0,0 +1,13 @@
+package pl.wszib.oemdatabase_test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemDataBaseTest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OemDataBaseTest3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wszib/oemdatabase_test3/OemDataBaseTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.oemdatabase_test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OemDataBaseTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
