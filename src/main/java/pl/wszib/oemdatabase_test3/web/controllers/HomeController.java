09db42935728b5cc0f1a8d4dbf728fb40db21f6b
Marcin <lmartino@wp.pl>
2023-08-12 12:19:43 +0200

    Pierwsza strona

---
+++ src/main/java/pl/wszib/oemdatabase_test3/web/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package pl.wszib.oemdatabase_test3.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("firstName", "Adam");
+        return "homePage";
+    }
+}
